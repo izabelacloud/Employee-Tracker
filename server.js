a6e0ea9d315f06e67b60b3fed3a5ecc49dba7816
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-29 10:53:54 -0700

    working on add department

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+const departments = [];
+
 // const apiRoutes = require('./routes/apiRoutes');
 
 // Express middleware
@@ -66,6 +69,74 @@ showAllDepartments = () => {
 
 
 
+//function to show all roles
+showAllRoles = () => {
+    console.log('Showing all roles...\n');
+    //query to select all departments
+    const sql = `SELECT * FROM role`;
+
+    //execute query
+    db.promise().query(sql, (err, rows) => {
+        if(err) throw err;
+        console.table(rows);
+    })
+        //end executing query
+        db.end();
+};
+
+
+//function to show all employees
+showAllEmployees = () => {
+    console.log('Showing all employees...\n');
+    //query to select all employees
+    const sql = `SELECT * FROM employee`;
+
+    //execute query
+    db.promise().query(sql, (err, rows) => {
+        if(err) throw err;
+        console.table(rows);
+    })
+        //end executing query
+        db.end();
+};
+
+
+//function to add a Department
+addDepartment = (name) => {
+    console.log('Adding a department...\n');
+    // sql = 'INSERT INTO department SET ?';
+    // const params = [req.params.id];
+    // const query = db.query(
+    //   'INSERT INTO department SET ?',
+    //   {
+    //     name: 'test department name'
+    //   },
+    //   function(err, res) {
+    //     if (err) throw err;
+    //     console.log(res.affectedRows + ' department inserted!\n');
+    //     // Call updateProduct() AFTER the INSERT completes
+    //     // updateProduct();
+    //   }
+    // );
+    // // logs the actual query being run
+    // console.log(query.sql);
+
+    const sql = `INSERT INTO department (name) 
+    VALUES (?)`;
+    const params = [name];
+    // ES5 function, not arrow function, to use `this`
+    db.query(sql, params, function(err, result) {
+        if (err) throw err;
+        console.log(res.affectedRows + ' department inserted!\n');
+
+    // logs the actual query being run
+    console.log(query.sql);
+    });
+};
+
+
+
+
 //function to initiate the first set of questions to the user
 const promptInitialChoices = function() {
     inquirer.prompt([
@@ -84,14 +155,29 @@ const promptInitialChoices = function() {
         }
     ])
     .then((answers) => {
-        const{initialChoices} = answers;
+        const{initialChoices, departmentName} = answers;
 
         if(initialChoices === "View all departments"){
-
             //call a function to show all departments
             showAllDepartments();
-
         }
+
+        if(initialChoices === "View all roles"){
+            //call a function to show all roles
+            showAllRoles();
+        }
+
+        if(initialChoices === "View all employees"){
+            //call a function to show all roles
+            showAllEmployees();
+        }
+
+        if(initialChoices === "Add a department"){
+            //call a function to show all roles
+ 
+            addDepartment();
+        }
+
     })
 }
 
